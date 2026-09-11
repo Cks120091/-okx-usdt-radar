6f218d14b454024c164e67df3d029b66bbd53ec4
Cks120091 <k7v4srb5rc@privaterelay.appleid.com>
2026-09-12 00:04:22 +0800

    Bump shell cache for single-coin 15m history UI

--- radar/static/service-worker.js
+++ radar/static/service-worker.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "okx-radar-shell-v4.5-history-replay-1";
+const SHELL_CACHE = "okx-radar-shell-v4.6-single-coin-history-1";
 const SHELL_ASSETS = ["/", "/manifest.webmanifest", "/radar-icon.svg"];
 
 self.addEventListener("install", event => {
