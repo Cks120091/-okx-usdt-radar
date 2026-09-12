4439b226a0b08e2cf49274f3dd27ba570e0b97df
Cks120091 <k7v4srb5rc@privaterelay.appleid.com>
2026-09-12 22:07:25 +0800

    Keep existing service worker cache contract

--- radar/static/service-worker.js
+++ radar/static/service-worker.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "okx-radar-shell-v4.9-history-server-push-1";
+const SHELL_CACHE = "okx-radar-shell-v4.8-history-clear-all-1";
 const SHELL_ASSETS = ["/", "/manifest.webmanifest", "/radar-icon.svg"];
 
 self.addEventListener("install", event => {
