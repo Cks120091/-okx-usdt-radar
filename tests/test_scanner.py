efa18b2a457205726f83841d13cf0ddd3ea983e1
Cks120091 <k7v4srb5rc@privaterelay.appleid.com>
2026-09-13 22:15:04 +0800

    Re-export scanner test fixtures

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,5 +1,8 @@
 from tests import legacy_scanner_cases as _legacy
 
+for _name in dir(_legacy):
+    if not _name.startswith("__"):
+        globals()[_name] = getattr(_legacy, _name)
 
 ScannerMarketPulseTests = _legacy.ScannerMarketPulseTests
 
