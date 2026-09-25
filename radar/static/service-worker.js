cbc908aa141338bba9a74d5e348cbecbdf5817c8
Cks120091 <k7v4srb5rc@privaterelay.appleid.com>
2026-09-25 23:43:50 +0800

    Refresh shell cache for breakout retest and quality tabs

--- radar/static/service-worker.js
+++ radar/static/service-worker.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "okx-radar-shell-v4.20-zh-labels";
+const SHELL_CACHE = "okx-radar-shell-v4.21-breakout-retest-quality80";
 const SHELL_ASSETS = ["/", "/manifest.webmanifest", "/radar-icon.svg"];
 
 self.addEventListener("install", event => {
